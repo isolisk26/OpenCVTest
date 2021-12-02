9023976d7b73c0c573003ff813dadd5ce02794d2
LBC <bclee@medicalip.com>
2021-12-02 13:36:51 +0900

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by LBC on 2021/12/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
