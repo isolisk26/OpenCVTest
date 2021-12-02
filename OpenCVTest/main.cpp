704fb4a72c2d9c873f171426408dfb13a65c076f
LBC <bclee@medicalip.com>
2021-12-02 14:05:23 +0900

    ㅁㅁㄴㅇ

--- OpenCVTest/main.cpp
+++ OpenCVTest/main.cpp
@@ -6,9 +6,12 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    std::cout << "OpenCV Version" << CV_VERSION << std::endl;
     std::cout << "Hello, World!\n";
     return 0;
 }
